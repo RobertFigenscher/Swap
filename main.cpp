d35b1f72f749ca1c87cb27af4e2f1a12f2257d83
RobertFigenscher <robert.figenscher@upr.edu>
2024-10-16 19:24:38 +0000

    start

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 using namespace std;
+void swap(int &, int &)
 int main()
 {
+  int a, b;
+  a = 3;
+  b = 5;
+  cout << "a is " << a << "and b is " << b;
+  swap (a,b)
+  cout << "a is " << a << "and b is " << b;
   return 0;
+}
+
+void swap(int &c, int &d)
+{
+  int hold;
+  hold = c;
+  c = d;
+  d = hold;
+  return;
 }
